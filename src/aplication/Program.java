e02e93e45bc150a2ca4642459fc3921b5854939d
MarkSant <Santana.marco.a@gmail.com>
2020-04-24 13:46:17 -0300

    Modificações para suportar JPA

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,31 @@
 package aplication;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("courseJdbc");
+			EntityManager entityManager = entityManagerFactory.createEntityManager();
+	
+			//find
+			//Cliente cliente = entityManager(Cliente.class, 1);
+			
+			Cliente cliente = new Cliente(2, "Maria");
+		
+			entityManager.getTransaction().begin(); //inicia a transacao para certeza q dara certo
+			entityManager.persist(cliente);
+			//entityManager.flush() -faz com que a persistencia aconteca,mesmo sem iniciar a transacao
+			entityManager.getTransaction().commit();//termina  transacao
+			
+			entityManager.remove(
+			
+			entityManager.close();
+			entityManagerFactory.close();
+			
 	}
 
 }
